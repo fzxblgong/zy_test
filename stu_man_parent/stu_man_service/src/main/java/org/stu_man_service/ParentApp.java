e2eb40721c2a00d7e148d56cd3ed0e756f25d00f
Gongxl <test@163.com>
2019-12-29 12:49:14 +0800

    学生管理mavenweb项目eclipse版

---
+++ stu_man_parent/stu_man_service/src/main/java/org/stu_man_service/ParentApp.java
@@ -0,0 +1,16 @@
+package org.stu_man_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParentApp 
+{
+	public void test(){
+		System.out.println("service say hello.");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ stu_man_parent/stu_man_service/src/test/java/org/stu_man_service/ParentAppTest.java
@@ -0,0 +1,23 @@
+package org.stu_man_service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParentAppTest 
+{
+	public void test(){
+		System.out.println("say hello.");
+	}
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
